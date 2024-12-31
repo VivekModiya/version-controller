efa9af92193ff2d4b20ba5bda5b9ede2628f4581
vivek_2401 <vivek.m@iqm.com>
2025-01-01 00:12:25 +0530

    Added control Version

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,81 @@
+import React from 'react';
+import './styles.css';
+
 function App() {
-    return <>Hello</>;
+    const [currentControlVersion, setControlVersion] = React.useState('');
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const currentTab = tabs[0];
+
+        if (currentTab && currentTab.url) {
+            // Fetch cookies only for the current tab's URL
+            chrome.cookies.getAll({ url: currentTab.url }, (cookies) => {
+                const controlVersion =
+                    cookies.find((cookie) => cookie.name === 'control_version')
+                        ?.value ?? '';
+                setControlVersion(controlVersion); // Your function to handle the control version
+            });
+        }
+    });
+
+    const joinClassNames = (...args: (string | boolean)[]): string => {
+        return args.filter(Boolean).join(' ');
+    };
+
+    const handleVersionChange = (version: string) => {
+        if (version === currentControlVersion) {
+            return;
+        }
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const tab = tabs[0];
+            if (tab && tab.id) {
+                chrome?.scripting?.executeScript?.({
+                    target: {
+                        tabId: tab.id,
+                        allFrames: false,
+                    },
+                    func: (controlVersion: string) => {
+                        document.cookie = `control_version=${controlVersion}`;
+                        location.reload();
+                    },
+                    args: [version],
+                });
+                close();
+            }
+        });
+    };
+
+    return (
+        <div style={{ display: 'flex', gap: 10, padding: 2 }}>
+            <button
+                onClick={() => handleVersionChange('default')}
+                className={joinClassNames(
+                    'button',
+                    currentControlVersion === 'default' && 'selected'
+                )}
+            >
+                Default
+            </button>
+            <button
+                onClick={() => handleVersionChange('next')}
+                className={joinClassNames(
+                    'button',
+                    currentControlVersion === 'next' && 'selected'
+                )}
+            >
+                Next
+            </button>
+            <button
+                onClick={() => handleVersionChange('later')}
+                className={joinClassNames(
+                    'button',
+                    currentControlVersion === 'later' && 'selected'
+                )}
+            >
+                Later
+            </button>
+        </div>
+    );
 }
 
 export default App;
